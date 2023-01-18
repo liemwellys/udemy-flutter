b1480967157173f7b43d3c663571b51863d6d5c7
liemwellys <welly.eic06g@nctu.edu.tw>
2023-01-18 16:15:21 +0800

    V30: Adding Layout Widgets
    - add ElevatedButton Widgets
    - set null for each onPressed ElevatedButton
      since no method currently developed

--- 02-flutter-basics/flutter_complete_guide/lib/main.dart
+++ 02-flutter-basics/flutter_complete_guide/lib/main.dart
@@ -16,14 +16,36 @@ class MyApp extends StatelessWidget {
   // Widget Describes the configuration for an [Element].
   @override
   Widget build(BuildContext context) {
-    // home is core widget which Flutter will bring onto the screen
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
+      // home is core widget which Flutter will bring onto the screen
       // Scaffold is base page design,
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+
+            // set null onPressed for avoid error since no method currently developed
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            )
+          ],
+        ),
       ),
     );
   }
