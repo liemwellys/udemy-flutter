50433dad216fabc57131ac7090ddec9180bff815
liemwellys <welly.eic06g@nctu.edu.tw>
2023-02-07 16:43:37 +0800

    V115: Final Polishing
    set TransactionList container into 450

--- 04-widgets-styling-adding-logic/personal_expenses_app/lib/widgets/transaction_list.dart
+++ 04-widgets-styling-adding-logic/personal_expenses_app/lib/widgets/transaction_list.dart
@@ -16,7 +16,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 450,
       child: userTransactions.isEmpty
           ? Column(
               children: <Widget>[
